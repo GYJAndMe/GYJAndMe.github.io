3ca853a527f30a9f3a3545680faf3120f140dada
Tengfei Yang <tengfeiyang2010@gmail.com>
2019-08-10 17:34:02 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,31 +2,41 @@
 	Photon by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+	*/
 
-(function($) {
+	(function($) {
 
-	var	$window = $(window),
+		var	$window = $(window),
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
-		});
+	breakpoints({
+		xlarge:   [ '1141px',  '1680px' ],
+		large:    [ '981px',   '1140px' ],
+		medium:   [ '737px',   '980px'  ],
+		small:    [ '481px',   '736px'  ],
+		xsmall:   [ '321px',   '480px'  ],
+		xxsmall:  [ null,      '320px'  ]
+	});
 
+
+	document.addEventListener('WeixinJSBridgeReady', function() {
+		document.getElementById('audios').play()
+	})
+
+	document.addEventListener('touchstart', function() {
+		document.getElementById('audios').play()
+	})
+	
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
 
 	// Scrolly.
-		$('.scrolly').scrolly();
+	$('.scrolly').scrolly();
 
 })(jQuery);
